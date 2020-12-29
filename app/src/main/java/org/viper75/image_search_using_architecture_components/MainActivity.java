8a9ef29d7536748ea6819e61a3b5a3f1c5778a7a
viper75 <sean.huvaya03@yahoo.com>
2020-12-29 20:14:30 +0200

    Initial commit

---
+++ app/src/main/java/org/viper75/image_search_using_architecture_components/MainActivity.java
@@ -0,0 +1,14 @@
+package org.viper75.image_search_using_architecture_components;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
